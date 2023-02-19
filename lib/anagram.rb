c6cf01f696e8404dff6132d56439e16fbcc8677a
Isaac Ongera <isaacongera717@gmail.com>
2023-02-19 10:18:09 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        
+        @word = word
+    end
+
+    def match(list)
+        list.filter do |myword|
+            myword.chars.sort == @word.chars.sort
+        end
+    end
+end
